ee06c6fe29d07696485df8595e51fc17b734f728
prasagungg <agungprasetio878@gmail.com>
2022-02-26 23:22:11 +0700

    user detail & repo

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -4,11 +4,12 @@ import githubRedurecer from "./GithubReducer";
 const GithubContext = createContext();
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
   };
 
@@ -22,9 +23,9 @@ export const GithubProvider = ({ children }) => {
     });
 
     const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
+      // headers: {
+      //   Authorization: `token ${GITHUB_TOKEN}`,
+      // },
     });
 
     const { items } = await response.json();
@@ -32,6 +33,48 @@ export const GithubProvider = ({ children }) => {
     dispatch({
       type: "GET_USERS",
       payload: items,
+    });
+  };
+
+  const getUser = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`);
+
+    if (response.status === 404) {
+      window.location = "/notfound";
+    } else {
+      const data = await response.json();
+
+      dispatch({
+        type: "GET_USER",
+        payload: data,
+      });
+    }
+  };
+
+  const getUserRepos = async (login) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      sort: "created",
+      per_page: 10,
+    });
+
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`,
+      {
+        // headers: {
+        //   Authorization: `token ${GITHUB_TOKEN}`,
+        // },
+      }
+    );
+
+    const data = await response.json();
+
+    dispatch({
+      type: "GET_REPOS",
+      payload: data,
     });
   };
 
@@ -44,8 +87,12 @@ export const GithubProvider = ({ children }) => {
       value={{
         users: state.users,
         loading: state.loading,
+        user: state.user,
+        repos: state.repos,
         searchUsers,
+        getUser,
         clearUsers,
+        getUserRepos,
       }}
     >
       {children}
